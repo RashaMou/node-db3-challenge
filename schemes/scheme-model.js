fcbdd30f534eb89f62e9dd02dc8710fc1322bf1c
Rasha Moumneh <rasha@fastmail.com>
2019-12-11 17:38:50 -0500

    findSteps request

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,8 @@ const db = require("../data/db-config");
 
 module.exports = {
   find,
-  findById
-  // findSteps,
+  findById,
+  findSteps
   // add,
   // update,
   // remove
@@ -17,4 +17,27 @@ function findById(id) {
   return db("schemes")
     .where({ id })
     .first();
+}
+
+/* 
+select s.id, 
+      s.step_number, 
+      s.instructions, 
+      sch.scheme_name 
+      from steps as s
+join schemes as sch 
+  on s.scheme_id = sch.id
+where s.scheme_id = 2
+*/
+
+function findSteps(schemeId) {
+  return db("steps")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .where("steps.scheme_id", schemeId);
 }
